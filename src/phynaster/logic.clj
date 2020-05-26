031059b48301d30a6128e50c1e300e0414259039
ysmiraak <ysmiraak@gmail.com>
2020-05-26 19:22:45 +0200

    naive finite domain constraints

--- src/phynaster/logic.clj
+++ src/phynaster/logic.clj
@@ -1,6 +1,10 @@
 (ns phynaster.logic)
 
-;; Term = LVar | Value
+;; Term = LVar | Value | Seq Term
+;;
+;; u v w : Term
+;; p q r : LVar
+;; x y z : Value
 ;;
 ;; a term is either a logical variable (lvar) or a value.
 ;; a lvar is implemented as a vector/list whose head is ::lvar,
@@ -12,8 +16,8 @@
 ;; one must supply an id of another type.
 
 (defn lvar [id] [::lvar id])
-(defn lvar? [x] (and (sequential? x) (= (first x) ::lvar)))
-(defn toseq [x] (and (sequential? x) (seq x)))
+(defn lvar? [u] (and (sequential? u) (= (first u) ::lvar)))
+(defn toseq [u] (and (sequential? u) (seq u)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;; unification rules ;;
@@ -103,6 +107,13 @@
 (defprotocol IConstraint
   (capply [this state]))
 
+(defn ccheck [{:keys [cset] :as state}]
+  (reduce (fn [?state constraint]
+            (if-let [state ?state]
+              (capply constraint state)
+              (reduced nil)))
+          state cset))
+
 (defrecord C=!= [u v]
   ;; TODO commutative, u v order does not matter, can also be extend to multivariate
   IConstraint
@@ -122,11 +133,43 @@
         (capply state)
         make-node)))
 
-(defn ccheck [{:keys [cset] :as state}]
-  (reduce (fn [?state constraint]
-            (when-let [state ?state]
-              (capply constraint state)))
-          state cset))
+(defrecord Cdomain [u dom]
+  ;; dom : Set Value
+  IConstraint
+  (capply [this {:keys [smap] :as state}]
+    (let [u (walk u smap)]
+      (cond
+        ;; unbound, keep for future
+        (lvar? u)         (update state :cset conj this)
+        ;; satisfied, throw constraint away
+        (contains? dom u) (update state :cset disj this)
+        ;; violated, fail
+        ))))
+
+(defn domain [u dom]
+  (fn [state]
+    (-> (Cdomain. u dom)
+        (capply state)
+        make-node)))
+
+(defrecord Cunique [qs]
+  IConstraint
+  (capply [this {:keys [smap] :as state}]
+    (let [us (map #(walk % smap) qs)]
+      (cond
+        ;; violated, fail
+        (not (apply distinct? us)) nil
+        ;; unbound, keep for future
+        (some lvar? us) (update state :cset conj this)
+        ;; satisfied, throw constraint away
+        :else           (update state :cset disj this)
+        ))))
+
+(defn unique [& qs]
+  (fn [state]
+    (-> (Cunique. (vec qs))
+        (capply state)
+        make-node)))
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; basic constructs ;;
@@ -139,8 +182,8 @@
 ;; Goal = State -> INode
 ;;
 ;; === : Term * Term -> Node
-;; disjoin : Goal * Goal -> INode
-;; conjoin : Goal * Goal -> INode
+;; disjoin : Goal * Goal -> Goal
+;; conjoin : Goal * Goal -> Goal
 
 (defrecord State [smap nvar cset])
 (def initial-state (State. {} 0 #{}))
@@ -154,8 +197,16 @@
 (defn disjoin [goal goal'] (fn [state] (plus (goal state) (goal' state))))
 (defn conjoin [goal goal'] (fn [state] (bind (goal state) goal')))
 
-(defn g1 [state] (unit state))
 (defn g0 [state] zero)
+(defn g1 [state] (unit state))
+
+(defn any [gs]
+  (reduce (fn [g g'] (fn [state] (plus (fn [] (g state)) (g' state))))
+          g0 gs))
+
+(defn all [gs]
+  (reduce (fn [g g'] (fn [state] (bind (fn [] (g state)) g')))
+          g1 gs))
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; user interface ;;
@@ -256,3 +307,61 @@
 ;; - transparent representation
 ;; - make operators operads
 ;; - reify constraints
+
+
+
+
+(comment
+
+  (def hints
+    [2 0 7 0 1 0 5 0 8
+     0 0 0 6 7 8 0 0 0
+     8 0 0 0 0 0 0 0 6
+     0 7 0 9 0 6 0 5 0
+     4 9 0 0 0 0 0 1 3
+     0 3 0 4 0 1 0 2 0
+     5 0 0 0 0 0 0 0 1
+     0 0 0 2 9 4 0 0 0
+     3 0 6 0 8 0 4 0 9])
+
+  (defn make-cells [hints]
+    (map (fn [cell hint]
+           (if (zero? hint)
+             (lvar cell)
+             hint))
+         (for [r (range 9)
+               c (range 9)]
+           [r c])
+         hints))
+
+  (defn make-nines [cells]
+    (let [rows (partition 9 cells)
+          cols (apply map list rows)
+          subs (for [r3c9 (partition 3 rows)
+                     r3c3 (partition 3 (apply map list r3c9))]
+                 (apply concat r3c3))]
+      (concat rows cols subs)))
+
+  (defn sudoku [hints]
+    (let [cells (make-cells hints)
+          nines (make-nines cells)]
+      (->> (run* [q]
+             (all (map (partial apply unique) nines))
+             (all (keep (fn [cell]
+                          (when (lvar? cell)
+                            (any (map (partial === cell)
+                                      (range 1 10)))))
+                        cells))
+             (=== q cells))
+           (map :q))))
+
+  (defn to-grid [cells]
+    (mapv vec (partition 9 cells)))
+
+  (-> hints sudoku first to-grid)
+
+  (doseq [res (sudoku hints)]
+    (clojure.pprint/pprint
+     (to-grid res)))
+
+  )
